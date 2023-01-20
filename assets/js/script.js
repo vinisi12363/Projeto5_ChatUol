5533a986c65c77569189d20251b73f8d074cbb9b
vinisi12363 <viniciuspv.si@gmail.com>
2023-01-20 15:51:18 -0300

    versao com o get axios implementada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,96 @@
-const user = new Object();
-user.nome = prompt("Digite o seu nome de usuário");
-     
-console.log(user);
 
-        const promise=axios.post('https://mock-api.driven.com.br/api/v6/uol/participants' , user.nome);
-        promise.then (processResponse);
+
+
+
+let mensagem = new Object();
+
+const user = new Object();
+
+function criarUser(){
+        user.name = prompt("Digite o seu nome de usuário");
+
+        const promise=axios.post('https://mock-api.driven.com.br/api/v6/uol/participants' , user);
+        promise.then(processResponse);
         promise.catch(processError);
         console.log("Enviou a requisição");
 
-function processResponse(resposta){
-        console.log("Voltou a resposta");
-        console.log (resposta.status);
+
+        
 }
+criarUser();
+
+function carregarMensagem(){
+
+    const  loadMessages = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    loadMessages.then(createChat);
+    loadMessages.catch(processError);
+}
+carregarMensagem();
+function processResponse(resposta){
+
+        console.log("usuario cadastrado!");
+        console.log (resposta.status);
+
+}
+
 
 function processError(erro) {
         console.log("Status code: " + erro.response.status); // Ex: 404
         console.log("Mensagem de erro: " + erro.response.data); // Ex: Not Found
 }
 
+
+
+
+
+
+function statusResponse(resposta){
+        console.log("online!");
+
+}
+
+
+function createChat(resposta){
+        let chatContent=document.querySelector('.chatContainer');
+        chatContent.innerText = "";
+    
+        for(let i = 0;  i < 100 ; i ++){
+            let hora= resposta.data[i].time;
+            let nome_from= resposta.data[i].from;
+            let nome_to= resposta.data[i].to;
+            let  texto= resposta.data[i].text;
+            let tipo= resposta.data[i].type;
+            
+            if (tipo === 'status' || tipo === 'message'){
+                chatContent.innerHTML += `
+                    <div data-test = "message" class="${tipo}">
+                    (${hora}) ${nome_from} para ${nome_to} :${texto}
+                    </div> 
+    
+                `
+            }
+        }
+    
+        chatContent.querySelector('div:last-child').scrollIntoView();
+}
+
+
+
+
+
+
+
+function verifyStatus(){
+        verificadorDeStatus=user;
+        const promise= axios.post('https://mock-api.driven.com.br/api/v6/uol/status',verificadorDeStatus);
+        promise.then(statusResponse);
+        promise.catch(processError);
+}
+setInterval(() => {
+    console.log("verificando status do usuario: "+user.name);
+    verifyStatus();
+}, 5000);
+
+setInterval(() => {
+    carregarMensagem();  
+}, 3000);
