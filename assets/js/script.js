c30c31afd4abe4791c908dc6c307eb6099da7f6b
vinisi12363 <viniciuspv.si@gmail.com>
2023-01-22 22:03:36 -0300

    removendo a gui de login após alguns bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,77 @@
 "use strict";
 let mensagem = new Object();
-const user = new Object();
+let user = new Object();
 
-let usuarioAutorizado = 0;
+
+let usuarioAutenticado = 0;
+
+let chatContent=document.querySelector('.chatContainer');
+let heightPage = chatContent.scrollHeight;
+              
+
+autenticarUsuario();
+
 
 function autenticarUsuario(){
 
-    let inputUsuario = document.getElementById('inputLogin');
+    let inputUsuario = prompt("Digite o nome de usuário");
 
-    if(usuarioAutorizado == 0 && (inputUsuario.value!= null || inputUsuario.value != undefined|| inputUsuario.value !== "")){
-            user.name = inputUsuario.value;
+
+    if(usuarioAutenticado == 0 && (inputUsuario!= null || inputUsuario != undefined|| inputUsuario!== "")){
+            user.name = inputUsuario;
             const  authUser = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
             authUser.then(verificaUsuarioExiste);
             authUser.catch(processErrorAutenticador);
     }else{
             alert("Nome de usuário já existe ou o campo está vazio, por favor escolha outro!");
-            inputUsuario.value="";
+            inputUsuario="";
+            autenticarUsuario();
     }
 }
 
 
 function verificaUsuarioExiste(resposta){
-            
-            const mainInterface= document.querySelector(".main");
-            const loginInterface = document.querySelector (".loginScreen");
-            for(let i = 0;  i < 100 ; i ++){
-                if(user.name === resposta.data[i].from){
+
+         //debugger;  
+      
+        for (let i=0 ; i<100 ; i++){
+                if(resposta.data[i].from===user.name){
                     spam("o nome de usuario ja existe, tente um outro!");
                     user.name ="";
-                }else if (user.name!= undefined || user.name !="" || user.name != resposta.data[i].from){
-                    console.log("logado com sucesso!");
-                    setTimeout(() => {
-                        loginInterface.classList.add("hide");
-                        mainInterface.classList.remove("hide");
-                    }, 1000);
-                    
-                    usuarioAutorizado = 1;
+                    autenticarUsuario();
+                
+                }else if (user.name!= undefined || user.name !="" || resposta.data[i].from!=user.name){
                    
+                    console.log("logado com sucesso!");
+                    usuarioAutenticado = 1;
+                    criarUser();
+                    break;
                 }else{
-                    console.log("caiu no else da veirificação");
+                    console.log("caiu no else da verificação");
                 }
-            }
+        }
 }
 
 
-if (usuarioAutorizado == 1){
-    criarUser();
-}
+
 function carregarMensagem(){
+
             const  loadMessages = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
             loadMessages.then(createChat);
             loadMessages.catch(processErrorLoadMessages);
+            chatContent.scrollIntoView(false);
 }
 
 function criarUser(){
           
-            if(usuarioAutorizado== 1){
+           
             const creatingUser= axios.post('https://mock-api.driven.com.br/api/v6/uol/participants' , user);
             creatingUser.then(processResponseCreateUser);
             creatingUser.catch(processErrorCreateUser);
             carregarMensagem();
+           
             
-            }else{
-            console.log("houve um problema com a autenticação do usuario, por favor volte mais tarde.");
-            
-        }
+        
 }
 
 
@@ -76,35 +83,11 @@ function processResponseCreateUser(resposta){
 
 }
 
-
-function processErrorLoadMessages(erro) {
-            console.log("Status code: " + erro.response.status); // Ex: 404
-            console.log("erro no loadMessages " + erro.response.data); // Ex: Not Found
-}
-
-function processErrorAutenticador(erro) {
-            console.log("Status code: " + erro.response.status); // Ex: 404
-            console.log("erro no autenticador " + erro.response.data); // Ex: Not Found
-
-}function processErrorCreateUser(erro) {
-            console.log("Status code: " + erro.response.status); // Ex: 404
-            console.log("Erro no create User: " + erro.response.data); // Ex: Not Found
-}
-function statusResponse(){
-            console.log("online!");
-
-}
-function processErrorStatus(erro){
-            console.log("Status code: " + erro.response.status); // Ex: 404
-            console.log("Erro no status: " + erro.response.data); // Ex: Not Found
-}
-
-
 function createChat(resposta){
-            let chatContent=document.querySelector('.chatContainer');
+           
+            
             chatContent.innerText = "";
-            let heightPage = chatContent.scrollHeight;
-                 
+               
             for(let i = 0;  i < 100 ; i ++){
                 let hora= resposta.data[i].time;
                 let nome_from= resposta.data[i].from;
@@ -134,27 +117,17 @@ function createChat(resposta){
 
             }  
             
-            chatContent.scrollTop = chatContent.scrollHeight;
+            chatContent.scrollIntoView(false);
 }
 
 
-
 function verifyStatus(){
-            const promise= axios.post('https://mock-api.driven.com.br/api/v6/uol/status',user);
+            const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', user);
             promise.then(statusResponse);
             promise.catch(processErrorStatus);
 }
 
-setInterval(() => {
-            verifyStatus();          
-  
-}, 5000);
 
-setInterval(() => {
-    
-            carregarMensagem();  
-   
-}, 3000); 
 
 
 document.addEventListener("keypress", function (e){
@@ -178,12 +151,54 @@ function sendMessage(msg){
             promise.then(processResponseSendMessage);
             promise.catch(processErrorSendMessage);
             carregarMensagem();
+            chatContent.scrollIntoView(false);
 }
 
-function processResponseSendMessage(resposta){
+
+    
+setInterval(() => {
+    verifyStatus();          
+
+}, 5000);
+
+
+
+setInterval(() => {
+
+    carregarMensagem();  
+    chatContent.scrollIntoView(false);
+}, 3000); 
+
+
+function processErrorLoadMessages(erro) {
+    console.log("Status code: " + erro.response.status); // Ex: 404
+    console.log("erro no loadMessages " + erro.response.data); // Ex: Not Found
+}
+function processErrorAutenticador(erro) {
+    console.log("Status code: " + erro.response.status); // Ex: 404
+    console.log("erro no autenticador " + erro.response.data); // Ex: Not Found
+
+}
+function processErrorCreateUser(erro) {
+    console.log("Status code: " + erro.response.status); // Ex: 404
+    console.log("erro no create " + erro.response.data); // Ex: Not Found
+
+}
+function statusResponse(){
+    console.log("online!");
+}
+function processErrorStatus(erro){
+    console.log("Status code: " + erro.response.status); // Ex: 404
+    console.log("erro no status " + erro.response.data); // Ex: Not Found
+
+}
+
+function processResponseSendMessage(){
     console.log("mensagem enviada com sucesso!");
 }
+
 function processErrorSendMessage(erro){
     console.log("Status code: " + erro.response.status); // Ex: 404
-    console.log("Erro no envio de mensagens: " + erro.response.data); // Ex: Not Found
+    console.log("erro no SendMessage " + erro.response.data); // Ex: Not Found
+
 }
